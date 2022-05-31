4a2798d416c19ab3b37e51d67a69c0471161ee04
Suan Nyan <suanthnjobs@gmail.com>
2022-05-31 11:30:15 +0630

    master[update the code for the index and css]

---
+++ resources/views/userside/index.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+    <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Suan Thiha Nyan/ My Portfolio website</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/common.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/reset.css') }}" rel="stylesheet">
+        <!-- Styles -->
+
+
+    </head>
+    <body class="antialiased">
+      <div class="div-banner-website">
+        <div class="div-li-banner">
+          <ul class="ul-alltabs">
+            <li>Home</li>
+            <li>About</li>
+            <li>Project and Work Experiences</li>
+            <li>My high-level educational paths</li>
+            <li>Contact Me</li>
+            <li></li>
+          </ul>
+
+        </div>
+      </div>
+
+      <div class="div-background-index">
+        <div class="div-">
+
+        </div>
+      </div>
+    </body>
+</html>
---
+++ resources/views/userside/profile.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.master')
+@section('content')
+<div class="div-whole-profile">
+  <div></div>
+</div>
+@endsection
